c881462fcf8ff8a97cc20a0b94fb8e3edfc2faee
john <linjw@inspur.com>
2018-01-26 18:46:15 +0800

    1/26

--- src/main.js
+++ src/main.js
@@ -6,19 +6,39 @@ const app = express();
 app.use('/', express.static('out'))
 
 app.all('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
 });
 
-app.post('/analysisTs', function (req, res) {
-  console.info('[server][/analysisTs]')
-  require('./bin/analysisTs').test()
-  res.send('132')
+app.get('/favicon.ico', function(req, res) {
+	res.status(204);
+});
 
+var fs = require('fs');
+var Busboy = require('busboy');
+app.post('/analysisTs', function (req, res) {
+	let busboy = new Busboy({ headers: req.headers });
+	busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+		var saveTo = path.join('.', filename);
+		console.log('Uploading: ' + saveTo);
+
+		file.on('data', function(data) {
+        	console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
+      	});
+      	// file.on('end', function() {
+       //  	console.log('File [' + fieldname + '] Finished');
+      	// });
+	});
+	busboy.on('finish', function() {
+		console.log('Upload complete2');
+		res.writeHead(200, { 'Connection': 'close' });
+		res.end("That's all folks!");
+	});
+	req.pipe(busboy);
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+	console.log('Example app listening on port 3000!');
 });
--- src/ui/component/fileInput.vue
+++ src/ui/component/fileInput.vue
@@ -7,24 +7,33 @@
 export default {
   methods : {
     fileChange () {
-      let file = this.$el.files[0];
+      
 
-      $.ajax({
-        type: 'POST',
-        url: `http://${SERVER_HOST}/analysisTs`,
-        crossDomain: true,
-        data: '{"some":"json"}',
-        dataType: 'json',
-        success: function(responseData, textStatus, jqXHR) {
-          var value = responseData.someKey;
-        },
-        error: function (responseData, textStatus, errorThrown) {
-          alert('POST failed.');
-        }
-      });
-      
-      this.$emit('change')
-      
+      if( window.FormData !== undefined ) 
+      //make sure that we can use FormData ie>9, chrome > 7, opera > 12 safari >5, android > 3  gecko mobile > 2, opera mobile >12 <- wil support XHR too
+      {
+        let form = new FormData()
+        let file = this.$el.files[0];  
+        form.append('userFile', file)
+        $.ajax(
+          {
+            type: 'POST',
+            url: `http://${SERVER_HOST}/analysisTs`,
+            crossDomain: true,
+            data: form,
+            success: function(responseData, textStatus, jqXHR) {
+              console.error(`success, status = ${textStatus}` )
+            },
+            error: function (responseData, textStatus, errorThrown) {
+              console.error(`fail, status = ${textStatus}, error = + ${errorThrown}` )
+            },
+            cache: false,
+            contentType: false,
+            processData: false
+          }
+        );
+        this.$emit('change')
+      }
     }
   }
 }
